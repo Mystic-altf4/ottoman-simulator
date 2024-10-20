1aaf5133537a9fa9098dacf60550d4c8be7f52ff
Aram <89261203+Mystic-altf4@users.noreply.github.com>
2024-10-20 11:28:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+meslek = input("Ne olmak istersiniz? ")
+if meslek == "Reaya" or meslek == "Reaya".lower() or meslek == "Reaya".upper():
+    print("Tamam, git ve tarım yap köle!")
+elif meslek == "Sadrazam" or meslek == "Sadrazam".lower() or meslek == "Sadrazam".upper():
+    print("Padişah hasta, git ve Serdar-I Ekrem ol köle!")
+elif meslek == "Vezir" or meslek == "Vezir".upper() or meslek == "Vezir".lower():
+    print("Hangi Vezir?")
+    print("Böyle kalırsın işte")
+elif meslek == "Nişancı" or meslek == "Nişancı".upper() or meslek == "Nişancı".lower():
+    print("Tuğramı çek bakayım köle")
+elif meslek == "Defterdar" or meslek == "Defterdar".upper() or meslek == "Defterdar".lower():
+    print("Mısır, Trablusgarp, Cezayir, Bosna Hersek (tanıdık mı geldi tezeko dede), Hırvatistan alındı; Hakkari, Kosova, Kuveyt, Yemen, İran (çok tanıdık), Bağdat kaybedildi. Dahası da ekte köle")
+elif meslek == "Kazasker" or meslek == "Kazasker".upper() or meslek == "Kazasker".lower():
+    print("Tüm kadılar ve müderrisler vebaya yakalandı yenilerini ata köle")
+elif meslek == "Şeyhülislam" or meslek == "Şeyhülislam".lower() or meslek == "Şeyhülislam".upper():
+    print("Yazııık bir işe yaradığını mı sanıyorsun? LAİK DEVLET LAİK TÜRKİYE (evet atatürkçüyüz)")
+elif meslek == "Reisülküttap" or meslek == "Reisülküttap".lower() or meslek == "Reisülküttap".upper():
+    print("Yaz kızım")
+elif meslek == "Kaptan-I Derya" or meslek == "Kaptan-I Derya".upper() or meslek == "Kaptan-I Derya".lower():
+    print("Üzgünüm, denizde savaş çıkmış Divan'a gelme sen") 
+elif meslek == "New Cherry" or meslek == "New Cherry".lower() or meslek == "New Cherry".upper() or meslek == "Yeni Kiraz" or meslek == "Yeni Kiraz".lower() or meslek == "Yeni Kiraz".upper():
+    print("Siz de bedavaya para almayın artık ya israf israf")
+elif meslek == "Sol Ulufeciler" or meslek == "Sol Ulufeciler".lower() or meslek == "Sol Ulufeciler".upper() or meslek == "Sağ Ulufeciler" or meslek == "Sağ Ulufeciler".lower() or meslek == "Sağ Ulufeciler".upper():
+    print("Bayrağı taşırken bayıldın ve devlet çöktü. Aynen devam")
+elif meslek == "Bukayo Saka" or meslek == "Bukayo Saka".lower() or meslek == "Bukayo Saka".upper():
+    print("Savaş konusunda çok yeteneksizsin ve sadece su taşıyorsun ama çok iyi futbolcusun")
+elif meslek == "Bizans" or meslek == "Bizans".lower() or meslek == "Bizans".upper():
+    print("ahahahahahajshakakdjdjsljdcj Fatih SAPLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+elif meslek == "Beylikler" or meslek == "Beylikler".upper() or meslek == "Beylikler".lower():
+    print("ahahaskhakdhskdhskfhdskdajhsksjdfh Yavuz SAPLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+elif meslek == "Padişah" or meslek == "Padişah".upper() or meslek == "Padişah".lower():
+    sultan = input("Hangi Padişah Olmak İstersiniz? ")
+    if sultan == "Osman Bey" or sultan == "Osman Bey".upper() or sultan == "Osman Bey".lower() or sultan == "Osman Gazi"  or sultan == "Osman Gazi".upper() or sultan == "Osman Gazi".lower():
+        print( "Tebrikler! Bursa'yı göremeden öldün") 
+    elif sultan == "Orhan Bey" or sultan == "Orhan Bey".upper() or sultan == "Orhan Bey".lower() or sultan == "Orhan Gazi"  or sultan == "Orhan Gazi".upper() or sultan == "Orhan Gazi".lower():
+        print( "Tebrikler! Kötüleyemediğim cevapsın! İyi son yippee afferin sana")
+    elif sultan == "1. Murat" or sultan == "1. Murat".upper() or sultan == "1. Murat".lower() or sultan == "Hüdavendigar"  or sultan == "Hüdavendigar".upper() or sultan == "Hüdavendigar".lower():
+        print( "Tebrikler! Sırplar sizi hançerledi")
+    elif sultan == "1. Bayezid" or sultan == "1. Bayezid".upper() or sultan == "1. Bayezid".lower() or sultan == "Yıldırım Bayezid"  or sultan == "Yıldırım Bayezid".upper() or sultan == "Yıldırım Bayezid".lower():
+        print( "Tebrikler! Timur'a esir düştün")
+    elif sultan == "1. Mehmed" or sultan == "1. Mehmed".upper() or sultan == "1. Mehmed".lower() or sultan == "Çelebi Mehmet"  or sultan == "Çelebi Mehmet".upper() or sultan == "Çelebi Mehmet".lower():
+        print( "Tebrikler! Tek işin fetret devrini bitirmek ama yine de aferin")
+    elif sultan == "2. Murat" or sultan == "2. Murat".upper() or sultan == "2. Murat".lower():
+        print( "Tebrikler! Fatih de olsa tahtını isteyerek 13 yaşında bebeye bırakacak kadar enayiydin")
+    elif sultan == "2. Mehmed" or sultan == "2. Mehmed".upper() or sultan == "2. Mehmed".lower() or sultan == "Fatih Sultan Mehmed"  or sultan == "Fatih Sultan Mehmed".upper() or sultan == "Fatih Sultan Mehmed".lower():
+        print( "Tebrikler! Kötüleyemediğim cevapsın! İyi son yippee afferin sana")
+    elif sultan == "2. Bayezid" or sultan == "2. Bayezid".upper() or sultan == "2. Bayezid".lower():
+        print( "Tebrikler! Yükselme döneminde durağan şekilde devlet yönettin")
+    elif sultan == "1. Selim" or sultan == "1. Selim".upper() or sultan == "1. Selim".lower() or sultan == "Yavuz Sultan Selim"  or sultan == "Yavuz Sultan Selim".upper() or sultan == "Yavuz Sultan Selim".lower():
+        print( "Tebrikler! Kötüleyemediğim cevapsın! İyi son yippee afferin sana")
+    elif sultan == "1. Süleyman" or sultan == "1. Süleyman".upper() or sultan == "1. Süleyman".lower() or sultan == "Kanuni Sultan Süleyman"  or sultan == "Kanuni Sultan Süleyman".upper() or sultan == "Kanuni Sultan Süleyman".lower():
+        print( "Tebrikler! Kötüleyemediğim cevapsın! İyi son yippee afferin sana")
+    elif sultan == "2. Selim" or sultan == "2. Selim".upper() or sultan == "2. Selim".lower() or sultan == "Sarı Selim"  or sultan == "Sarı Selim".upper() or sultan == "Sarı Selim".lower():
+        print( "Tebrikler! Sefere çıkmayan ilk padişahsın")
+    elif sultan == "1. Mustafa" or sultan == "1. Mustafa".upper() or sultan == "1. Mustafa".lower():
+        print( "Tebrikler! Delisin!")
+    elif sultan == "2. Osman" or sultan == "2. Osman".upper() or sultan == "2. Osman".lower() or sultan == "Genç Osman"  or sultan == "Genç Osman".upper() or sultan == "Genç Osman".lower():
+        print( "Tebrikler! Yeni kirazlar seni tahttan indirdi")
+    elif sultan == "İbrahim" or sultan == "İbrahim".upper() or sultan == "İbrahim".lower():
+        print( "Tebrikler! Şeyhülislam seni tahttan indirdi")
+    elif sultan == "2. Mustafa" or sultan == "2. Mustafa".upper() or sultan == "2. Mustafa".lower():
+        print( "Tebrikler! İlk defa büyük toprak kaybettin")
+    elif sultan == "3. Ahmed" or sultan == "3. Ahmed".upper() or sultan == "3. Ahmed".lower():
+        print( "Tebrikler! Lale Devri başladı")
+    elif sultan == "3. Selim" or sultan == "3. Selim".upper() or sultan == "3. Selim".lower():
+        print( "Tebrikler! Reformların o kadar kötüydü ki şehit edildin")
+    elif sultan == "4. Mustafa" or sultan == "4. Mustafa".upper() or sultan == "4. Mustafa".lower():
+        print( "Tebrikler! Şehit edilen padişah tarafından idam edildin")
+    elif sultan == "2. Mahmud" or sultan == "2. Mahmud".upper() or sultan == "2. Mahmud".lower():
+        print( "Tebrikler! Yeni kiraz Ocağını yok ettin ama seni tahttan indirecek bir güç olmaması da mantıksız değil")
+    elif sultan == "Abdülmecid" or sultan == "Abdülmecid".upper() or sultan == "Abdülmecid".lower():
+        print( "Tebrikler! Demokratikleşme çalışmaları yaptın ama çok da başarılı değillerdi")
+    elif sultan == "Abdülaziz" or sultan == "Abdülaziz".upper() or sultan == "Abdülaziz".lower():
+        print( "Tebrikler! Tahttan indirilince intihar edecek kadar hırslıydın")
+    elif sultan == "5. Murad" or sultan == "5. Murad".upper() or sultan == "5. Murad".lower():
+        print( "Tebrikler! En kısa süre boyunca tahtta kaldın")
+    elif sultan == "2. Abdülhamid" or sultan == "2. Abdülhamid".upper() or sultan == "2. Abdülhamid".lower():
+        print( "Tebrikler! Beylerbeyi Sarayı'na kapatıldın")
+    elif sultan == "6. Mehmed" or sultan == "6. Mehmed".upper() or sultan == "6.Mehmed".lower() or sultan == "Vahdettin"  or sultan == "Vahdettin".upper() or sultan == "Vahdettin".lower():
+        print( "o kadar kötüydün ki devleti Ermeniler, İtalyanlar, Fransızlar ve Yunanlara verdin. İngilizlerden rüşvet aldın, İstanbul'dan ayrıldın, sürgün edildin ve iyi ki öldün ve saltanatla halivelik kaldırıldı")
+    else:
+        print("Burada olmayan ve önemli başarısızlığı ve başarısı olmayan 3. Mehmed ya da 1. Ahmed gibi padişah girmemeyi dene")
+else:
+    print("Sana Osmanlı'yı öğretmeyeceğim")
+time.sleep(10)
